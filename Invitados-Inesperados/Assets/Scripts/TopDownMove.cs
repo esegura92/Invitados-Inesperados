be08d7b116a44587a0e0d67cdc14d5d559f5039f
Emilio Segura <esegura92@dunritegames.gg>
2019-01-26 12:24:06 -0500

    First Commit. Project set

---
+++ Invitados-Inesperados/Assets/Scripts/TopDownMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TopDownMove : MonoBehaviour
+{
+    public float walkSpeed;
+    private float curSpeed;
+    private float maxSpeed;
+    private Rigidbody2D rigidBody2D;
+
+    public float speed;
+
+    private void Awake()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// Call on fixed Update
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    public void Move(float x, float y)
+    {
+        curSpeed = walkSpeed;
+        maxSpeed = curSpeed;
+
+        // Move senteces
+        rigidBody2D.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * curSpeed, 0.8f),
+                                             Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, 0.8f));
+    }
+}
