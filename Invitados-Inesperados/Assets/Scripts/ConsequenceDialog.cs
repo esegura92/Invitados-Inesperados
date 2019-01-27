b5824acf1ada251b57bbdca83823c922595fb5a2
Emilio Segura <esegura92@dunritegames.gg>
2019-01-27 03:17:44 -0500

    acto 4 prefab y nuevo tipo de dialogo

---
+++ Invitados-Inesperados/Assets/Scripts/ConsequenceDialog.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsequenceDialog : Dialog
+{
+    public Dialog[] concequencesDialogs;
+
+    public void SetConsequence()
+    {
+        nextDialog = concequencesDialogs[AppManager.Instance.miedometro];
+    }
+}
